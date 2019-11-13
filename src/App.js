e52e85b3f9bdc8636dbb37c8eba7b0b2a3b8f3c6
iawen28 <marcie.ellen.anderson@gmail.com>
2019-11-13 10:30:01 -0800

    bubbles progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './App.css';
-import TicTacToe from './components/TicTacToe'
+import TicTacToe from './components/TicTacToe';
+import Bubble from './components/Bubble';
 
 function App() {
   return (
     <div className="App">
-      <TicTacToe gamesize="48vw" bgcolor="#3A6162" gamecolor="#A1C3AF" heading="Tic Tac Toe" />
+      {/*<TicTacToe gamesize="48vw" bgcolor="#3A6162" gamecolor="#A1C3AF" heading="Tic Tac Toe" />*/}
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      <Bubble />
+      
     </div>
   );
 }
---
+++ src/components/Bubble.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Bubble.css';
+
+function Bubble() {
+  return (
+    <div className="bubble">
+      <div className="bubble-main-color" />
+      <div className="bubble-alt-color" />
+    </div>
+  );
+}
+
+export default Bubble;
